ee94815e2513ee65be1b9685524ed44b629b3d15
youngUnPark <mduddns@naver.com>
2021-10-15 23:57:52 +0900

    feat: JPA Auditing 클래스 추가

---
+++ src/main/java/com/givememoney/entity/BaseTime.java
@@ -0,0 +1,25 @@
+package com.givememoney.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class) //JPA에게 해당 Entity는 Auditing기능을 사용한다는 것을 알리는 어노테이션입니다.
+public class BaseTime {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+}
